0f6074811969fef1735cc37b77d3504ab102cbf2
Suhail Suhail <suhailece@gmail.com>
2026-04-08 11:02:39 -0700

    First working template

--- lua/merrin.lrplugin/Config.lua
+++ lua/merrin.lrplugin/Config.lua
@@ -6,45 +6,105 @@ return {
                 {
                     name = "Valentina Reneff-Olson",
                     rules = {
-                        { field = "keywords", op = "contains", value = "@vallady" },
+                        { field = "keywords", op = "any", value = "@vallady" },
                     },
                     children = {
                         {
                             name = "Catwoman",
                             rules = {
-                                { field = "searchableIptc", op = "contains", value = "Catwoman" },
+                                { field = "iptc", op = "any", value = "Catwoman" },
                             },
                             smart = {
                                 {
                                     name = "RAWs",
-                                    match = "all",
                                     rules = {
-                                        { field = "fileType", op = "is", value = "Raw" },
+                                        { field = "fileFormat", op = "==", value = "RAW" },
                                     },
                                 },
                                 {
                                     name = "Rated",
-                                    match = "all",
                                     rules = {
                                         { field = "rating", op = ">=", value = 1 },
                                     },
                                 },
                                 {
                                     name = "Five Star",
-                                    match = "all",
                                     rules = {
                                         { field = "rating", op = "==", value = 5 },
                                     },
                                 },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+
+        {
+            name = "Locations",
+            children = {
+                {
+                    name = "Walt Disney Concert Hall",
+                    rules = {
+                        { field = "iptc", op = "any", value = "Walt Disney Concert Hall" },
+                    },
+                    children = {
+                        {
+                            name = "Catwoman",
+                            rules = {
+                                { field = "iptc", op = "any", value = "Catwoman" },
+                            },
+                            smart = {
                                 {
-                                    name = "Edits",
-                                    match = "any",
+                                    name = "RAWs",
                                     rules = {
-                                        { field = "fileType", op = "is", value = "TIFF" },
-                                        { field = "fileType", op = "is", value = "PSD" },
-                                        { field = "fileType", op = "is", value = "PSB" },
+                                        { field = "fileFormat", op = "==", value = "RAW" },
                                     },
                                 },
+                                {
+                                    name = "Rated",
+                                    rules = {
+                                        { field = "rating", op = ">=", value = 1 },
+                                    },
+                                },
+                                {
+                                    name = "Five Star",
+                                    rules = {
+                                        { field = "rating", op = "==", value = 5 },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+
+        {
+            name = "Projects",
+            children = {
+                {
+                    name = "Catwoman",
+                    rules = {
+                        { field = "jobIdentifier", op = "==", value = "Catwoman" },
+                    },
+                    smart = {
+                        {
+                            name = "RAWs",
+                            rules = {
+                                { field = "fileFormat", op = "==", value = "RAW" },
+                            },
+                        },
+                        {
+                            name = "Rated",
+                            rules = {
+                                { field = "rating", op = ">=", value = 1 },
+                            },
+                        },
+                        {
+                            name = "Five Star",
+                            rules = {
+                                { field = "rating", op = "==", value = 5 },
                             },
                         },
                     },
